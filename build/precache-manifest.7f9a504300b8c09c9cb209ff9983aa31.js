348dd0bb1b99aef5e727096291e9f39432093286
Tyler Hawkins <tylerhawkins@workfront.com>
2020-11-14 14:34:53 -0700

    chore: builds v1 of the app

---
+++ build/precache-manifest.7f9a504300b8c09c9cb209ff9983aa31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf036b854272b8b1d5533de267da92c",
+    "url": "/puppy-playdate/build/index.html"
+  },
+  {
+    "revision": "9a10ec55dac51b559c7c",
+    "url": "/puppy-playdate/build/static/css/main.1c9a59e7.chunk.css"
+  },
+  {
+    "revision": "851c93195f1ef3cec9a1",
+    "url": "/puppy-playdate/build/static/js/2.5bec4314.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/puppy-playdate/build/static/js/2.5bec4314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a10ec55dac51b559c7c",
+    "url": "/puppy-playdate/build/static/js/main.8470e4d0.chunk.js"
+  },
+  {
+    "revision": "52ad71044295a33c5ddb",
+    "url": "/puppy-playdate/build/static/js/runtime-main.f6392aeb.js"
+  }
+]);
